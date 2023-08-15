cee0d4154ed00059eb603966ee3cea182b45b32a
SeoJungHan <seojh5939@gmail.com>
2023-08-15 12:17:44 +0900

    [FEAT] : ⚙ 할일목록생성 기능구현 완료

--- notLame/app/src/main/java/bootcamp/sparta/notlame/myPage/MyPageActivity.kt
+++ notLame/app/src/main/java/bootcamp/sparta/notlame/myPage/MyPageActivity.kt
@@ -2,11 +2,48 @@ package bootcamp.sparta.notlame.myPage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import bootcamp.sparta.notlame.R
 
 class MyPageActivity : AppCompatActivity() {
+    private lateinit var todoEditText : EditText // 핢일 추가하기 EditText
+    private lateinit var todoBtn : Button // 할일목록 입력 버튼
+    private lateinit var todoCheckBoxList : ArrayList<CheckBox>
+    private lateinit var view: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.my_page_activity)
+        view = LayoutInflater.from(this).inflate(R.layout.my_page_activity, null)
+        setContentView(view)
+        initView()
+        todoBtnOnClickListener()
+    }
+
+    // 할일 목록 추가버튼 클릭시 동작
+    private fun todoBtnOnClickListener() {
+        todoBtn.setOnClickListener {
+
+            // EditText 입력값이 있는지 체크
+            if (todoEditText.text.length <= 0) {
+                Toast.makeText(this, "할일을 추가하시려면 텍스트를 입력해주세요!", Toast.LENGTH_SHORT).show()
+            }
+
+            //CheckBoxList에서 visibility가 invisible인 제일 처음 값 가져온 후 EditText의 값 입력 후 visibility 상태변경
+            todoCheckBoxList.find { it.visibility == View.INVISIBLE }?.let {
+                it.text = todoEditText.text.toString()
+                it.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun initView() {
+        todoEditText = findViewById(R.id.mypage_et_todo)
+        todoBtn  = findViewById(R.id.mypage_btn_todo)
+        todoCheckBoxList = getCheckBoxList(view)
     }
 }
