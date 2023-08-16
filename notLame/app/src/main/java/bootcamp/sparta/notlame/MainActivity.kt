d6757a3e66cac3f4a9f5a9bc845765840abc2216
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-16 20:34:14 +0900

    [REMOVE] : ✂️ 더미 데이터 삭제

--- notLame/app/src/main/java/bootcamp/sparta/notlame/MainActivity.kt
+++ notLame/app/src/main/java/bootcamp/sparta/notlame/MainActivity.kt
@@ -1,6 +1,5 @@
 package bootcamp.sparta.notlame
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,23 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val textSubject = "제목1 제목1 제목1"
-        val textContent = "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
-                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
-                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
-                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.\" +\n" +
-                "                \"여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.\" +\n" +
-                "                \"여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.\" +\n" +
-                "                \"여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다."
-
-        val detailPage = findViewById<Button>(R.id.detailPage)
-        detailPage.setOnClickListener {
-            val intent = Intent(this, DetailPage::class.java)
-            intent.putExtra("detailSubject", textSubject)
-            intent.putExtra("detailContent", textContent)
-            startActivity(intent)
-        }
-
     }
 }
