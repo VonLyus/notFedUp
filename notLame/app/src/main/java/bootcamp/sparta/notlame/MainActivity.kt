730b38c609aa9dea1482ce136b07984510081de9
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-16 17:19:01 +0900

    [DESIGN] : 🎨 detailpage 글자크기, 버튼 색 변경

--- notLame/app/src/main/java/bootcamp/sparta/notlame/MainActivity.kt
+++ notLame/app/src/main/java/bootcamp/sparta/notlame/MainActivity.kt
@@ -10,5 +10,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textSubject = "제목1 제목1 제목1"
+        val textContent = "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
+                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
+                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다." +
+                "여기에 내용이 입력됩니다.여기에 내용이 입력됩니다.여기에 내용이 입력됩니다."
+
+        val detailPage = findViewById<Button>(R.id.detailPage)
+        detailPage.setOnClickListener {
+            val intent = Intent(this, DetailPage::class.java)
+            intent.putExtra("detailSubject", textSubject)
+            intent.putExtra("detailContent", textContent)
+            startActivity(intent)
+        }
+
     }
 }
