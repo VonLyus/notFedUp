bd27f40220664558847f43b5dcd072856b2340b8
AppCode <lus322@yonsei.ac.kr>
2023-08-17 20:13:47 +0900

    기능 수정

--- app/src/main/java/bootcamp/sparta/notlame/signIn/SignInPageActivity.kt
+++ app/src/main/java/bootcamp/sparta/notlame/signIn/SignInPageActivity.kt
@@ -30,19 +30,43 @@ class SignInPageActivity : AppCompatActivity() {
             val id = editTextId.text.toString()
             val password = editTextPassword.text.toString()
 
+            //값을 받아내야함
+
+            val checkName = intent.getStringExtra("userName") ?: "name"
+            val checkId = intent.getStringExtra("userId") ?: "id"
+            val checkPw = intent.getStringExtra("userPw") ?: "pw"
+            val checkTel = intent.getStringExtra("userTel") ?: "tel"
+            val checkPosition = intent.getStringExtra("userPosition") ?: "position"
+            val checkImage = intent.getIntExtra("userImage", 0)
+
+            //값 판단이 안되어 있음
+
             if (id.isEmpty() || password.isEmpty()) {
-                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
-            } else {
+
+                Toast.makeText(this, "아이디/비밀번호 둘 중 하나가 입력이 비어있습니다.", Toast.LENGTH_SHORT).show()
+            } else if((id.equals(checkId)) && (password.equals(checkPw))){
+
                 Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
 
-                // Get the user information from SharedPreferences
-                val sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
-                val name = sharedPrefs.getString("userName", "이름 없음")
-                val age = sharedPrefs.getString("userAge", "나이 없음")
-                val mbti = sharedPrefs.getString("userMBTI", "MBTI 없음")
+//                // Get the user information from SharedPreferences
+//                val sharedPrefs = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+//                val name = sharedPrefs.getString("userName", "이름 없음")
+//                val age = sharedPrefs.getString("userAge", "나이 없음")
+//                val mbti = sharedPrefs.getString("userMBTI", "MBTI 없음")
+// intent로 값을 넘기는 것으로 정했어서 우선 수정하겠습니다
 
                 val intent = Intent(this, MainPageActivity::class.java)
+
+                intent.putExtra("userName", checkName)
+                intent.putExtra("userId", checkId)
+                intent.putExtra("userPw", checkPw)
+                intent.putExtra("userTel", checkTel)
+                intent.putExtra("userPosition", checkPosition)
+                intent.putExtra("userImage", checkImage)
                 startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
             }
         }
 
